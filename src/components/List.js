db3c0731cc249aa6b9c8ea59e434ccf126f46f10
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2023-01-07 14:15:11 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -3,12 +3,17 @@ import ListItems from './ListItems';
 
  const List = ({listx}) =>{
   return(
-    // code here
-    
-      <ListItems  />
-    
-    
+        // code here
+        <>
+          <ul>
+            <li id="list" className='items'>
+              {listx.map((list) => (<ListItems valuex = {list}/>))}
+            </li>
+          </ul>
+          
+        </>
   )
 }
 
 export default List;
+Footer
